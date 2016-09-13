7333eb21b3cc7172607415de8ea46d96173a14c1
efremovich <sasha.fima@gmail.com>
2016-09-13 17:37:13 +0300

    Готовим Cookie

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,8 +14,41 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", handler)
 	http.HandleFunc("/video-upload", upload)
 	log.Fatal(http.ListenAndServe(":9090", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://192.168.1.14")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Mx-ReqToken,X-Requested-With")
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(204)
+	} else if r.Method == "POST" {
+		// body, _ := ioutil.ReadAll(r.Body)
+		// fmt.Println(string(body))
+		client := GetClient()
+		url := "http://192.168.1.14" + r.URL.Path
+		req, err := http.NewRequest("POST", url, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		req.Header = r.Header
+		req.Body = r.Body
+		for i, cookie := range r.Cookies() {
+			req.Cookies(cookie)
+		}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+		}
+		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(body))
+	}
+	fmt.Fprintf(w, "Барев, метод запроса: %s", r.Method)
+	fmt.Println(r.Method)
 }
 
 // upload logic
